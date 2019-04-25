db0f687c3ce2a6896d07b5c657589c3e351e8704
Rutuj Gavankar <rsgavank@iu.edu>
2019-04-25 14:13:39 -0400

    added new phase plots

---
+++ pendulum.m
@@ -0,0 +1,21 @@
+[x_0, y_0] = meshgrid(-4:.1:4, -3:.1:3);
+
+g = 9.81;
+
+dxdt = y_0; 
+dydt = - g*cos(x_0);
+
+H = 0.5*y_0.^2 - g*cos(x_0);
+
+quiver(x_0,y_0,dxdt,dydt)
+hold on;
+contour(x_0,y_0,H,20, 'LineWidth', 2)
+hold off
+xlabel('\theta(t)')
+ylabel('\omega(t)')
+title('Phase portrait of a non-linear pendulum')
+axis([-4 4 -3 3])
+
+
+
+

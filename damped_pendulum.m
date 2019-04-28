55309a2bf7137f003558f065d6b96eb548d1feef
Rutuj Gavankar <rsgavank@iu.edu>
2019-04-28 14:03:39 -0400

    Final version

--- damped_pendulum.m
+++ damped_pendulum.m
@@ -1,7 +1,7 @@
 clc;clear;
 %------------ODE---------------------------
-g = 9.81;
-b = 1.5;
+g = 1;
+b = 4;
 f = @(t,Y) [Y(2);- b*Y(2) - g*sin(Y(1))];
 
 [x_0,y] = meshgrid(-2*pi:.1:2*pi, -4:.1:4);
@@ -10,27 +10,22 @@ dxdt = y;
 dydt = - b*y - g*sin(x_0);
 %--------------Y(t)-----------------------
 figure(1)
-[t, Y_a] = ode45(f, [0 1000], [-1 0]);
+[t, Y_a] = ode45(f, [0 1000], [0 3.2]);
 plot(Y_a(:,1),Y_a(:,2),'linewidth',2.5)
 hold on
-[t, Y_a] = ode45(f, [0 1000], [-3 0]);
+[t, Y_a] = ode45(f, [0 1000], [pi 0]);
 plot(Y_a(:,1),Y_a(:,2),'linewidth',2.5)
-[t, Y_a] = ode45(f, [0 1000], [3 3]);
+[t, Y_a] = ode45(f, [0 1000], [-3 3]);
 plot(Y_a(:,1),Y_a(:,2),'linewidth',2.5)
-[t, Y_a] = ode45(f, [0 1000], [5 -2.4]);
-plot(Y_a(:,1),Y_a(:,2),'linewidth',2)
-[t, Y_a] = ode45(f, [0 1000], [-2.5 -2.4]);
-plot(Y_a(:,1),Y_a(:,2),'linewidth',2.5)
-[t, Y_a] = ode45(f, [0 1000], [-5 -2.4]);
-plot(Y_a(:,1),Y_a(:,2),'linewidth',2.5)
+
 %--------Vector Field--------------------
 quiver(x_0,y,dxdt,dydt)
-title('Phase plot of a damped pendulum with b = 1')
+title('Phase plot of a damped pendulum with b = 4')
 xlabel('\theta(t)')
 ylabel('\omega(t)')
 axis([-2*pi 2*pi -4 4])
 %---------Hamiltonian--------------------
-contour(x_0,y,H,10, 'LineWidth',1, 'LineColor', 'k')
+contour(x_0,y,H,5, 'LineWidth',1, 'LineColor', 'k')
 grid on
 hold off
-legend('(\theta, \omega) = (-1,0)','(\theta, \omega) = (-3,0)','(\theta, \omega) = (3,3)', '(\theta, \omega) = (5,-2.4)', '(\theta, \omega) = (-2.5,-2.4)', '(\theta, \omega) = (-5,-2.4)', 'Vector Field', 'Hamiltonian')
+legend('(\theta, \omega) = (0,3.2)','(\theta, \omega) = (\pi,0)','(\theta, \omega) = (-3,3)', 'Vector Field', 'Hamiltonian')
